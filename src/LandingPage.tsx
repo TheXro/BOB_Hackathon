cd08c0d0ce1b756af6ae52cf690d4441bd677f4e
Ravi Yadav <ravyyadav97@gmail.com>
2024-06-29 23:15:46 +0530

    Update LandingPage.tsx

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -38,7 +38,7 @@ export default function LandingPage() {
         <Divider />
         <Highlights />
         <Divider />
-        <Pricing />
+       {/* <Pricing />*/}
         <Divider />
         <FAQ />
         <Divider />
